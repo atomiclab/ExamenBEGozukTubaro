a4f651a2b7d0fe5e55fc55cd524d1365a950582a
atomiclab <ginotubaro@gmail.com>
2024-03-29 19:18:42 -0300

    un par mas de cosas

--- src/test/java/com/example/TPGozukTubaro/service/implementation/OdontologoServiceTest.java
+++ src/test/java/com/example/TPGozukTubaro/service/implementation/OdontologoServiceTest.java
@@ -8,15 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class OdontologoServiceTest {
     @Autowired
     private IOdontologoService odontologoService;
+
     @Test
     void guardar() {
-        Odontologo odontologoTest = new Odontologo("NicolasG","Testeo","1234TEST");
+        Odontologo odontologoTest = new Odontologo("NicolasG", "Testeo", "1234TEST");
         odontologoService.guardar(odontologoTest);
         Odontologo odontologoTestRecienAgregado = odontologoService.buscarPorId(odontologoTest.getId());
 
@@ -25,7 +27,7 @@ class OdontologoServiceTest {
 
     @Test
     void actualizar() {
-        Odontologo odontologoTest = new Odontologo("NicolasA","Testeo","1234TEST");
+        Odontologo odontologoTest = new Odontologo("NicolasA", "Testeo", "1234TEST");
         odontologoService.guardar(odontologoTest);
         Odontologo odontologoTestRecienAgregado = odontologoService.buscarPorId(odontologoTest.getId());
 
@@ -39,7 +41,7 @@ class OdontologoServiceTest {
 
     @Test
     void eliminar() {
-        Odontologo odontologoTest = new Odontologo("NicolasE","Testeo","1234TEST");
+        Odontologo odontologoTest = new Odontologo("NicolasE", "Testeo", "1234TEST");
         odontologoService.guardar(odontologoTest);
         // Odontologo odontologoTestRecienAgregado = odontologoService.buscarPorId(odontologoTest.getId());
 
@@ -49,20 +51,33 @@ class OdontologoServiceTest {
         //Odontologo odontologoRecienEliminado = odontologoService.buscarPorId(odontologoTest.getId());
         // Verifica que el odontólogo haya sido eliminado correctamente
         // Se tuvo que cambiar el test porque ahora tiramos una exception
-        assertThrows(ResourceNotFoundException.class, () -> {
-            odontologoService.buscarPorId(odontologoTest.getId());});
+        assertThrows(ResourceNotFoundException.class, () -> odontologoService.buscarPorId(odontologoTest.getId()));
 
         //assertTrue(odontologoRecienEliminado == null);
     }
+
     @Test
-    void listarOdontologos(){
-        Odontologo odontologoTest = new Odontologo("NicolasL","Testeo","1234TEST");
+    void listarOdontologos() {
+        Odontologo odontologoTest = new Odontologo("NicolasL", "Testeo", "1234TEST");
         odontologoService.guardar(odontologoTest);
-        Odontologo odontologoTest2 = new Odontologo("NicolasL2","Segundo","1234TEST");
+        Odontologo odontologoTest2 = new Odontologo("NicolasL2", "Segundo", "1234TEST");
         odontologoService.guardar(odontologoTest2);
 
         List<Odontologo> odontologos = odontologoService.listarTodos();
 
-        assertTrue(odontologos.size()>0);
+        assertTrue(odontologos.size() > 0);
     }
+    
+    @Test
+    void findByMatricula() {
+        Odontologo odontologoTest = new Odontologo("NicolasL", "Testeo", "123444");
+        odontologoService.guardar(odontologoTest);
+
+        Optional<Odontologo> odontologoEncontrado = odontologoService.findByMatricula(odontologoTest.getMatricula());
+        odontologoEncontrado.ifPresent(odontologo -> assertEquals(odontologoTest.getMatricula(), odontologo.getMatricula()));
+        odontologoService.eliminar(odontologoTest.getId());
+        //asi la prueba puede correr y luego no tira un error que el odontologo esta duplicado (o mas cantidad)
+    }
+
+
 }
